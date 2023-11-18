6b4869c421d9a1b93e7b2a55d6d332328e189373
Md. Najmuzzaman <naj06cse@yahoo.com>
2023-11-18 15:48:24 +0600

    Time: 81 ms (93.92%) | Memory: 39.5 MB (86.48%) - LeetSync

--- 338-counting-bits/counting-bits.cs
+++ 338-counting-bits/counting-bits.cs
@@ -4,7 +4,7 @@ public class Solution {
         arr[0]=0;
         for(int i=1;i<=n;i++)
         {
-            arr[i]=arr[i/2]+ (i&1);
+            arr[i]=arr[i/2]+ (i%2);
         }
         return arr;
     }
