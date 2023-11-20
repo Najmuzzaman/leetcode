4453f8f4f1c538f82045606c47af48106c3cb65e
Md. Najmuzzaman <naj06cse@yahoo.com>
2023-11-20 09:41:00 +0600

    Time: 21 ms (70.59%) | Memory: 26.6 MB (64.71%) - LeetSync

---
+++ 233-number-of-digit-one/number-of-digit-one.cs
@@ -0,0 +1,30 @@
+public class Solution {
+    public int CountDigitOne(int n) {
+        long count = 0;
+        long factor = 1;
+
+        while (n / factor > 0)
+        {
+            long currentDigit = (n / factor) % 10;
+            long before = n / (factor * 10);
+            long after = n % factor;
+
+            if (currentDigit == 0)
+            {
+                count += before * factor;
+            }
+            else if (currentDigit == 1)
+            {
+                count += before * factor + after + 1;
+            }
+            else
+            {
+                count += (before + 1) * factor;
+            }
+
+            factor *= 10;
+        }
+
+        return (int)count;
+    }
+}
