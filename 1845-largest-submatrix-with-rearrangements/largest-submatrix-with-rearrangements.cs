e9d41d7a11cb00a51e3dbae98acf500e73d62bbd
Md. Najmuzzaman <naj06cse@yahoo.com>
2023-11-26 03:35:44 -0800

    Time: 279 ms (25.00%) | Memory: 64 MB (100.00%) - LeetSync

---
+++ 1845-largest-submatrix-with-rearrangements/largest-submatrix-with-rearrangements.cs
@@ -0,0 +1,30 @@
+public class Solution {
+    public int LargestSubmatrix(int[][] matrix) {
+        if (matrix == null || matrix.Length == 0 || matrix[0].Length == 0)
+            return 0;
+
+        int m = matrix.Length;
+        int n = matrix[0].Length;
+        int ans = 0;
+        for (int row = 0; row < m; row++)
+        {
+            for (int col = 0; col < n; col++)
+            {
+                if (matrix[row][col] != 0 && row > 0)
+                {
+                    matrix[row][col] += matrix[row - 1][col];
+                }
+            }
+
+            int[] currRow =new int[n];
+            Array.Copy(matrix[row], currRow, n); 
+            Array.Sort(currRow);
+            Array.Reverse(currRow);
+            for (int i = 0; i < n; i++)
+            {
+                ans = Math.Max(ans, currRow[i] * (i + 1));
+            }
+        }
+        return ans;
+    }
+}
