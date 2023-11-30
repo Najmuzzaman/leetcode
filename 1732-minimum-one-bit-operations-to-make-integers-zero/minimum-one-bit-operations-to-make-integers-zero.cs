69dcf83b781b7da7d1b05440c3c1880437076fb9
Md. Najmuzzaman <naj06cse@yahoo.com>
2023-11-29 19:17:23 -0800

    Time: 29 ms (100.00%) | Memory: 27 MB (100.00%) - LeetSync

---
+++ 1732-minimum-one-bit-operations-to-make-integers-zero/minimum-one-bit-operations-to-make-integers-zero.cs
@@ -0,0 +1,16 @@
+public class Solution {
+    public int MinimumOneBitOperations(int n) {
+        if(n==0) return 0;
+        if(n==1) return 1;
+        if(n<0) return n;
+        int i=0;
+        int a=n;
+        while(a!=1)
+        {
+            i++;
+            a/=2;
+        }
+        if((1<<i)<=n) i++;
+        return ((1<<i)-1) - MinimumOneBitOperations(n-(1<<(i-1)));
+    }
+}
