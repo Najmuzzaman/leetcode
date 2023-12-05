db431d41ad5f1a3012d9a59ef91b30115cda72d5
Md. Najmuzzaman <naj06cse@yahoo.com>
2023-12-04 19:27:50 -0800

    Time: 19 ms (78.48%) | Memory: 26.7 MB (36.71%) - LeetSync

--- 1806-count-of-matches-in-tournament/count-of-matches-in-tournament.cs
+++ 1806-count-of-matches-in-tournament/count-of-matches-in-tournament.cs
@@ -1,20 +1,5 @@
 public class Solution {
     public int NumberOfMatches(int n) {
-        int ans=0;
-        while(n!=1)
-        {
-            if(n%2==1)
-            {
-                ans+=n/2;
-                n=n/2+1;
-               
-            }
-            else
-            {
-                ans+=n/2;
-                 n/=2;
-            }
-        }
-        return ans;
+        return n-1;
     }
 }
