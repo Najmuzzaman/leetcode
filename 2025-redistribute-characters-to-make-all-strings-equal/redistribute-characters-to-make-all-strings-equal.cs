6d730c07f641be00c435684893d305302a69028a
Md. Najmuzzaman <naj06cse@yahoo.com>
2023-12-29 19:34:41 -0800

    Time: 74 ms (100.00%) | Memory: 43.8 MB (17.39%) - LeetSync

---
+++ 2025-redistribute-characters-to-make-all-strings-equal/redistribute-characters-to-make-all-strings-equal.cs
@@ -0,0 +1,21 @@
+public class Solution {
+    public bool MakeEqual(string[] words) {
+        int n = words.Length;
+        int[] m=new int[100];
+        for(int i=0;i<n;i++)
+        {
+            foreach(char a in words[i])
+            {
+                m[a - 'a']++;
+            }
+        }
+        for (int i = 0; i < 100;i++)
+        {
+            if(m[i]!=0)
+            {
+                if (m[i] % n != 0) return false;
+            }
+        }
+        return true;
+    }
+}
