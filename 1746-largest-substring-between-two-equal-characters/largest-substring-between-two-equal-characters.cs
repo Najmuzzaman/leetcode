1512ca8bfdcdec60f863a1b4a5bac5db3c0ac694
Md. Najmuzzaman <naj06cse@yahoo.com>
2023-12-30 23:11:38 -0800

    Time: 39 ms (100.00%) | Memory: 37.1 MB (24.00%) - LeetSync

---
+++ 1746-largest-substring-between-two-equal-characters/largest-substring-between-two-equal-characters.cs
@@ -0,0 +1,21 @@
+public class Solution {
+    public int MaxLengthBetweenEqualCharacters(string s) {
+        int n = s.Length;
+        int[] m=new int[100];
+        int dis=0;
+        bool flag = false;
+        for(int i=0;i<n;i++)
+        {
+            if (m[s[i] - 'a']!=0)
+            {
+                int a=i-m[s[i] - 'a'];
+                if (dis < a)
+                    dis = a;
+                flag = true;
+            }
+            else
+                m[s[i] - 'a']=i+1;
+        }
+        return flag?dis:-1;
+    }
+}
