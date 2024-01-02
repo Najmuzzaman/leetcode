a62cb259f2e927eefb634790efc6510a31175e36
Md. Najmuzzaman <naj06cse@yahoo.com>
2024-01-02 09:31:31 -0800

    Time: 227 ms (5.41%) | Memory: 49 MB (8.11%) - LeetSync

--- 2724-convert-an-array-into-a-2d-array-with-conditions/convert-an-array-into-a-2d-array-with-conditions.cs
+++ 2724-convert-an-array-into-a-2d-array-with-conditions/convert-an-array-into-a-2d-array-with-conditions.cs
@@ -1,44 +1,27 @@
 public class Solution {
     public IList<IList<int>> FindMatrix(int[] nums) {
-        List<IList<int>> result = new List<IList<int>>();
         int n = nums.Length;
-
         // Count the frequency of each element in nums
         Dictionary<int, int> frequency = new Dictionary<int, int>();
         foreach (var num in nums)
         {
             if (frequency.ContainsKey(num))
-            {
                 frequency[num]++;
-            }
             else
-            {
                 frequency[num] = 1;
-            }
         }
-
         // Determine the number of rows for the matrix
         int numRows = frequency.Max(kv => kv.Value);
         List<int>[] matrix = new List<int>[numRows];
         for (int i = 0; i < numRows;i++ )
             matrix[i] = new List<int>();
-
         // Initialize the matrix with the unique elements in nums
         foreach (var key in frequency.Keys)
         {
             int count = frequency[key];
             for (int i = 0; i < count; i++)
-            {
                 matrix[i].Add(key);
-            }
         }
-
-        // Convert the matrix to a list of lists
-        foreach (var row in matrix)
-        {
-            result.Add(row.ToList());
-        }
-
-        return result;
+        return matrix.Cast<IList<int>>().ToList();
     }
 }
